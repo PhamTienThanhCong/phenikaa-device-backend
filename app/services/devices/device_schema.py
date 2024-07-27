6822f2c972abde573bd9767155d4395e80a2f544
phamtienthanhcong <congphamtienthanh@gmail.com>
2024-07-27 14:32:10 +0700

    setting config

--- app/services/devices/device_schema.py
+++ app/services/devices/device_schema.py
@@ -2,45 +2,50 @@ from pydantic import BaseModel
 
 
 class DeviceCoreSchema(BaseModel):
-  id: int
-  name: str
-  category: str
-  information: str
-  note: str
-  total: int
-  image: str
-  total_used: int
-  total_maintenance: int
-  is_active: int
+    id: int
+    name: str
+    category: str
+    information: str
+    note: str
+    total: int
+    image: str
+    total_used: int
+    total_maintenance: int
+    is_active: int
 
 
 class DeviceCreateSchema(BaseModel):
-  name: str
-  category: str
-  information: str | None
-  image: str | None
-  note: str | None
-  total: int
-  
+    name: str
+    category: str
+    information: str | None
+    image: str | None
+    note: str | None
+    total: int
+
+
 class DeviceCreateSchemaFull(DeviceCreateSchema):
-  presigned_url: str | None
+    presigned_url: str | None
+
 
 class DeviceCreateResponse(DeviceCoreSchema):
-  presigned_url: str | None
+    presigned_url: str | None
+
 
 class DeviceUpdateResponse(DeviceCoreSchema):
-  presigned_url: str | None
+    presigned_url: str | None
+
 
 class DeviceUpdateSchema(BaseModel):
-  name: str | None 
-  category: str | None 
-  information: str | None 
-  note: str | None 
-  image: str | None
-  total: int | None 
-  total_used: int | None 
-  total_maintenance: int | None 
-  is_active: bool | None 
+    name: str | None
+    category: str | None
+    information: str | None
+    note: str | None
+    image: str | None
+    total: int | None
+    total_used: int | None
+    total_maintenance: int | None
+    is_active: bool | None
+
 
 class DeviceUpdateSchemaFull(DeviceUpdateSchema):
-  presigned_url: str | None
+    presigned_url: str | None
--- app/services/devices/device_service.py
+++ app/services/devices/device_service.py
@@ -4,52 +4,63 @@ from fastapi.responses import JSONResponse
 
 from app.core.setting import get_setting
 from app.services.devices.device_model import Device
-from app.services.devices.device_schema import DeviceCreateSchema, DeviceCreateSchemaFull, DeviceUpdateSchema, DeviceUpdateSchemaFull
+from app.services.devices.device_schema import (
+    DeviceCreateSchemaFull,
+    DeviceUpdateSchemaFull,
+)
 
 import uuid
 
+
 class DeviceService:
-  def __init__(self):
-    self.base = get_setting()
-    pass
-  def get_heath(self):
-    return JSONResponse(status_code=200, content={"status": "UP"})
-  
-  def get_device(self, db: Session, device_id: int):
-    return db.query(Device).filter(Device.id == device_id).first()
-  
-  def get_devices(self, db: Session, skip: int = 0, limit: int = 100):
-    return db.query(Device).offset(skip).limit(limit).all() 
-  
-  def get_device_by_name(self, db: Session, name: str):
-    return db.query(Device).filter(Device.name == name).first()
-  
-  def create_device(self, db: Session, device: DeviceCreateSchemaFull):
-    if (device.image):
-      device.image = ""
-      device.presigned_url = str(uuid.uuid4())
+    def __init__(self):
+        self.base = get_setting()
+        pass
 
-    db_device = Device(**device.dict())
-    db.add(db_device)
-    db.commit()
-    db.refresh(db_device)
+    def get_heath(self):
+        return JSONResponse(status_code=200, content={"status": "UP"})
 
-    db_device.presigned_url = f"{self.base['base_url']}/v1/device/presigned_url/{db_device.presigned_url}"
+    def get_device(self, db: Session, device_id: int):
+        return db.query(Device).filter(Device.id == device_id).first()
 
-    return db_device
+    def get_devices(self, db: Session, skip: int = 0, limit: int = 100):
+        return db.query(Device).offset(skip).limit(limit).all()
 
-  def update_device(self, db: Session, device_id: int, device: DeviceUpdateSchemaFull):
-    if (device.image):
-      device.image = ""
-      device.presigned_url = str(uuid.uuid4())
-    db.query(Device).filter(Device.id == device_id).update(device.dict())
-    db.commit()
+    def get_device_by_name(self, db: Session, name: str):
+        return db.query(Device).filter(Device.name == name).first()
 
-    db_device = db.query(Device).filter(Device.id == device_id).first()
-    db_device.presigned_url = f"{self.base['base_url']}/v1/device/presigned_url/{db_device.presigned_url}"
-    return db_device
-  
-  def delete_device(self, db: Session, device_id: int):
-    db.query(Device).filter(Device.id == device_id).delete()
-    db.commit()
-    return {"message": "Device deleted successfully"}
+    def create_device(self, db: Session, device: DeviceCreateSchemaFull):
+        if device.image:
+            device.image = ""
+            device.presigned_url = str(uuid.uuid4())
+
+        db_device = Device(**device.dict())
+        db.add(db_device)
+        db.commit()
+        db.refresh(db_device)
+
+        db_device.presigned_url = (
+            f"{self.base['base_url']}/v1/device/presigned_url/{db_device.presigned_url}"
+        )
+
+        return db_device
+
+    def update_device(
+        self, db: Session, device_id: int, device: DeviceUpdateSchemaFull
+    ):
+        if device.image:
+            device.image = ""
+            device.presigned_url = str(uuid.uuid4())
+        db.query(Device).filter(Device.id == device_id).update(device.dict())
+        db.commit()
+
+        db_device = db.query(Device).filter(Device.id == device_id).first()
+        db_device.presigned_url = (
+            f"{self.base['base_url']}/v1/device/presigned_url/{db_device.presigned_url}"
+        )
+        return db_device
+
+    def delete_device(self, db: Session, device_id: int):
+        db.query(Device).filter(Device.id == device_id).delete()
+        db.commit()
+        return {"message": "Device deleted successfully"}
