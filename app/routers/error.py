15ee0a8edc3d8ef8af6d2082f20835a87c61ac80
Phạm Tiến Thành Công <cong.pttc@gmail.com>
2024-08-09 23:49:01 +0700

    remove detial

--- app/routers/error.py
+++ app/routers/error.py
@@ -6,19 +6,19 @@ router = APIRouter(prefix="/test-error", tags=["error"])
 
 @router.get("")
 def get_error():
-    raise HTTPException(status_code=500, detail="Error")
+    raise HTTPException(status_code=500)
 
 
 @router.post("")
 def get_error():
-    raise HTTPException(status_code=500, detail="Error")
+    raise HTTPException(status_code=500)
 
 
 @router.put("")
 def get_error():
-    raise HTTPException(status_code=500, detail="Error")
+    raise HTTPException(status_code=500)
 
 
 @router.delete("")
 def get_error():
-    raise HTTPException(status_code=500, detail="Error")
+    raise HTTPException(status_code=500)
