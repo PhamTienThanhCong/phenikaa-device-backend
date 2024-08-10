f2966efb777ad164f776c979ff6c5d5a25fa71f4
Phạm Tiến Thành Công <cong.pttc@gmail.com>
2024-08-10 13:56:12 +0700

    fic

--- app/routers/error.py
+++ app/routers/error.py
@@ -6,19 +6,19 @@ router = APIRouter(prefix="/test-error", tags=["error"])
 
 @router.get("")
 def get_error():
-    return 0/1
+    return 1
 
 
 @router.post("")
 def get_error():
-    return 0/1
+    return 1
 
 
 @router.put("")
 def get_error():
-    return 0/1
+    return 1
 
 
 @router.delete("")
 def get_error():
-    return 0/1
+    return 1
